360ee42bfd00a73d0dc9f78310ac1c1e651f5f1d
Sohebkhan <102939844+sohebkhan2023@users.noreply.github.com>
2023-04-21 10:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(str)
+{
+	var str1=str;
+	var newString ="";
+	for(var i=str.length-1;i>=0;i--)
+		{
+			newString += str[i];
+			
+		}
+	if(newString==str1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 
 }
 module.exports = palindrome
